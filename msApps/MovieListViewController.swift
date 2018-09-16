ada5e6f0748dc783530998fc0563560584bf106e
roeyHonig <roeyhonig94@gmail.com>
2018-09-16 15:02:57 +0300

    segue to movieList

---
+++ msApps/MovieListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieListViewController.swift
+//  msApps
+//
+//  Created by hackeru on 7 Tishri 5779.
+//  Copyright © 5779 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
