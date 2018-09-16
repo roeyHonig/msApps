bd1c8fced6131f88a8942f904a807f0d7efcc674
roeyHonig <roeyhonig94@gmail.com>
2018-09-16 14:11:40 +0300

    Initial Commit

---
+++ msApps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  msApps
+//
+//  Created by hackeru on 7 Tishri 5779.
+//  Copyright © 5779 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
